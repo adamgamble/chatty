6cbe3b2e1fff4b4877508dd6bbd3f39e0bef6761
Adam Gamble <adamgamble@gmail.com>
2013-05-14 19:20:39 -0500

    added angular

---
+++ app/assets/javascripts/posts_controller.js
@@ -0,0 +1,12 @@
+function PostsCtrl($scope) {
+  $scope.posts = [
+    {
+      body: "Now is the time for all good men to come to the aid of their country.",
+      username: "Adam Gamble"
+    },
+    {
+      body: "Now is the time for all good men to come to the aid of their country.",
+      username: "Josh Adams"
+    }
+  ]
+}
