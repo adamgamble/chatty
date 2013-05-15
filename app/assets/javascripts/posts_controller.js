007f72ebd4ce4e09646588cfa00f7799cfcf591e
Adam Gamble <adamgamble@gmail.com>
2013-05-14 23:40:10 -0500

    lots of tiny changes

--- app/assets/javascripts/posts_controller.js
+++ app/assets/javascripts/posts_controller.js
@@ -1,12 +1,21 @@
-function PostsCtrl($scope) {
-  $scope.posts = [
-    {
-      body: "Now is the time for all good men to come to the aid of their country.",
-      username: "Adam Gamble"
-    },
-    {
-      body: "Now is the time for all good men to come to the aid of their country.",
-      username: "Josh Adams"
-    }
-  ]
+var chatty = angular.module('chatty', []);
+
+chatty.factory('Posts', function() {
+  return [{username: "Adam Gamble", body: "now is the time for all good men to come to the aid of their country"}];
+});
+
+chatty.directive("newpost", function() {
+  return function(scope, element, attrs) {
+    element.bind("submit", function() {
+      scope.posts.push({username: "Adam Gamble", body: $('.chat-box').val()})
+      $('.chat-box').val("");
+      $('body').animate({scrollTop: $(document).height()});
+      scope.$apply();
+      $('a.embed').oembed();
+    });
+  }
+});
+
+function PostsCtrl($scope, Posts) {
+  $scope.posts = Posts;
 }
